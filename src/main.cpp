768f0e905eece2a360297262d8982352087637a9
nuxarch <misbaqul4sfi4k>
2022-10-17 09:45:08 +0700

    ditambahkan struct

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,44 @@
 #include <Arduino.h>
- 
-//koneksi sensor suhu
+
+// koneksi sensor suhu
 #define SUHU1 A0
 #define SUHU2 A1
 #define SUHU3 A2
-//variabel nilai data terbesar
+
+// variabel nilai data terbesar
 uint8_t datamax = 0;
-//variabel hasil konversi
-uint16_t dataT1 = 10, dataT2 = 11, dataT3 = 12; 
+// variabel hasil konversi
+uint16_t dataT1 = 10, dataT2 = 11, dataT3 = 12;
 
+struct tempSensor_t
+{
+  char *namasensor;
+  uint16_t data;
+};
+tempSensor_t Sensor1;
 
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 }
 
-void loop() {
-  dataT1 = analogRead(A0);
-  dataT2 = analogRead(A1);
-  dataT3 = analogRead(A2);
-  
+void loop()
+{
+  dataT1 = map(analogRead(A0), 0, 1023, 0, 100);
+  dataT2 = map(analogRead(A1), 0, 1023, 0, 100);
+  dataT3 = map(analogRead(A2), 0, 1023, 0, 100);
+
   // =====================================================================
   datamax = dataT1;
-  if (datamax < dataT2 )
+  if (datamax < dataT2)
   {
     datamax = dataT2;
   }
-  if (datamax < dataT3) 
+  if (datamax < dataT3)
   {
     datamax = dataT3;
   }
-  Serial.println("T1=["+String(dataT1)+"],T2=["+String(dataT2)+"], T3=["+String(dataT3)+"], nilai sensor terbesar: "+String(datamax));
+  Serial.println("T1=[" + String(dataT1) + "],T2=[" + String(dataT2) + "], T3=[" + String(dataT3) + "], nilai sensor terbesar: " + String(datamax));
   // =====================================================================
   delay(500);
 }
